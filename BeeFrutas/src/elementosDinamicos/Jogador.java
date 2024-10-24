3b8248e3e3e3f1f517063e1a26b7f165ea19537d
adam-gui <145728082+adam-gui@users.noreply.github.com>
2024-10-24 01:58:48 -0300

    Update Jogador.java

--- BeeFrutas/src/elementosDinamicos/Jogador.java
+++ BeeFrutas/src/elementosDinamicos/Jogador.java
@@ -2,45 +2,61 @@ package elementosDinamicos;
 
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
-
 import interfaceJogo.TelaInicial;
 
-public class Jogador extends JLabel{
-	/**
-	 * 
-	 */
+public class Jogador extends JLabel {
 	private static final long serialVersionUID = 1L;
 	protected int tamanhoIcone;
 	public int x;
 	public int y;
+	public Mochila mochila;
 	public String nome;
 	protected String urlIcone;
 	
-	public Jogador(int x, int y, int tamanhoIcone, int idJogador, String nome){
+	// Construtor ajustado para inicializar a mochila
+	public Jogador(int x, int y, int tamanhoIcone, int idJogador, String nome, int capacidadeMochila){
 		if(idJogador == 0) {
-			urlIcone = "/imagens/jogador0.png";}
+			urlIcone = "/imagens/jogador0.png";
+			mochila = new Mochila(capacidadeMochila, "Jogador0", tamanhoIcone);  // Inicializa a mochila do jogador 0
+		}
 		if(idJogador == 1) {
-			urlIcone = "/imagens/jogador1.png";}
+			urlIcone = "/imagens/jogador1.png";
+			mochila = new Mochila(capacidadeMochila, "Jogador1", tamanhoIcone);  // Inicializa a mochila do jogador 1
+		}
 		this.tamanhoIcone = tamanhoIcone;
 		this.x = x;
 		this.y = y;
 		this.nome = nome;
-		this.setBounds(tamanhoIcone*x, tamanhoIcone*y, tamanhoIcone, tamanhoIcone);
+		this.setBounds(tamanhoIcone * x, tamanhoIcone * y, tamanhoIcone, tamanhoIcone);
+		
+		// Carrega o ícone do jogador
 		ImageIcon image = new ImageIcon(TelaInicial.class.getResource(urlIcone));
 		image.setImage(image.getImage().getScaledInstance(tamanhoIcone, tamanhoIcone, 1));
 		this.setIcon(image);
-
-}
-	
-	public void moverJogador(int moverX, int moverY, int limiteMovimento) {
-		if (this.x+1 < limiteMovimento && moverX >= 0) {
-			this.x = this.x + moverX;}
-		if (this.x > 0 && moverX <= 0) {
-			this.x = this.x + moverX;}
-		if (this.y+1 < limiteMovimento && moverY >= 0) {
-			this.y = this.y + moverY;}
-		if (this.y > 0 && moverY <= 0) {
-			this.y = this.y + moverY;}
-		this.setBounds(tamanhoIcone*x, tamanhoIcone*y, tamanhoIcone, tamanhoIcone);
 	}
-} 
+
+	// Adiciona fruta na mochila se possível
+	public boolean pegarFruta(Fruta fruta) {
+		return mochila.adicionarFruta(fruta);
+	}
+
+	// Consome uma fruta da mochila
+	public Fruta consumirFruta() {
+		return mochila.consumirFruta();
+	}
+	
+	public Mochila getMochila() {
+        return mochila;
+    }
+	
+	// Método para mover o jogador dentro dos limites do mapa
+	public void moverJogador(int moverX, int moverY, int limiteMovimento) {
+		if (this.x + moverX < limiteMovimento && this.x + moverX >= 0) {
+			this.x = this.x + moverX;
+		}
+		if (this.y + moverY < limiteMovimento && this.y + moverY >= 0) {
+			this.y = this.y + moverY;
+		}
+		this.setBounds(tamanhoIcone * x, tamanhoIcone * y, tamanhoIcone, tamanhoIcone);
+	}
+}
